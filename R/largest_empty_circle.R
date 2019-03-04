ff8e84dead4a1b0c512f3689fffd4eb6b4e8b0b1
Wolfgang Hamer <hamer@geographie.uni-kiel.de>
2019-03-04 15:22:27 +0100

    removed duplicates

--- R/largest_empty_circle.R
+++ R/largest_empty_circle.R
@@ -5,6 +5,7 @@
 ##' @return a list containing the LEC_raster and the Nodes including the distance to the closest location
 ##' @import dplyr sf deldir gstat
 ##' @importFrom raster raster
+##' @importFrom sp CRS
 ##' @importFrom automap autoKrige
 ##' @importFrom magrittr %>% %<>%
 ##' @export largest_empty_circle
@@ -13,6 +14,7 @@
 ##' @examples
 ##' library(mapview)
 ##' library(readr)
+##' library(sf)
 ##' locations <- read_delim("./data/raw_data/BronzeAgeFortifications.csv", delim=";") %>% 
 ##'   st_as_sf(coords = c("xUTM", "yUTM"),crs = 32634)
 ##' 
@@ -24,19 +26,21 @@ largest_empty_circle <- function(points,raster="default"){
   xdat <- sf::st_coordinates(points)[,1]
   ydat <- sf::st_coordinates(points)[,2]
   
-  deld <-deldir(x=xdat,y=ydat)
+  deld <- deldir::deldir(x=xdat,y=ydat)
 
-  dd <- deld$dirsgs %>% 
-    st_as_sf(coords = c("x1", "y1"),crs = sf::st_crs(points))
+  dd <- dplyr::tibble(x = c(deld$dirsgs$x1,deld$dirsgs$x2),
+                      y = c(deld$dirsgs$y1,deld$dirsgs$y2)) %>% 
+    dplyr::distinct() %>% 
+    st_as_sf(coords = c("x", "y"),crs = sf::st_crs(points))
   
   dd %<>% dplyr::mutate(mindist = unlist(Map(function(x){min(sf::st_distance(dd[x,],points))},x=1:nrow(dd)))) %>% 
     dplyr::select(mindist)
     
   if(class(raster)=="character"){
-    raster <-raster::raster(nrows=100, ncols=100, 
+    raster <-raster::raster(nrows=200, ncols=200, 
                         xmn=st_bbox(points)$xmin, xmx=st_bbox(points)$xmax, 
                         ymn=st_bbox(points)$ymin, ymx=st_bbox(points)$ymax,
-                        crs = CRS((sf::st_crs(points))$proj4string))
+                        crs = sp::CRS((sf::st_crs(points))$proj4string))
     raster[] <- rep(1,length(raster[]))
   }
   
